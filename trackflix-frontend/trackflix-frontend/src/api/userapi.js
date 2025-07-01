216c1486f1993e23dc7ad3afff23065d3e0db55b
Robiul Hasan Jisan <robiulhasanjisan88@gmail.com>
2025-07-01 20:41:09 +0600

    Delete trackflix-frontend/trackflix-frontend directory

--- trackflix-frontend/trackflix-frontend/src/api/userapi.js
+++
@@ -1,9 +0,0 @@
-const API_URL = "http://localhost:5000/api/users";
-
-export const fetchUsers = async () => { 
-  const res = await fetch(API_URL);
-  if (!res.ok) {
-    throw new Error("Failed to fetch users");
-  }
-  return res.json();
-};
--- trackflix-frontend/trackflix-frontend/src/pages/Search.jsx
+++
@@ -1,9 +0,0 @@
-function Search() {
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">Search Results</h1>
-    </div>
-  );
-}
-
-export default Search;
