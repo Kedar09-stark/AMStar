28fbd54cac6d56f62073382bf65e34b73adca1ff
Robiul Hasan Jisan <robiulhasanjisan88@gmail.com>
2025-07-04 23:35:03 +0600

    test host firebaseAdmin.js

--- trackflix-backend/config/firebaseAdmin.js
+++ trackflix-backend/config/firebaseAdmin.js
@@ -12,21 +12,18 @@
 
 //module.exports = admin;
 
-    const admin = require("firebase-admin");
+ const admin = require("firebase-admin");
+const path = require("path");
 
-let serviceAccount;
+// Path to your Firebase service account JSON file
+const serviceAccountPath = path.join(__dirname, "..", "serviceAccountKey.json");
 
-if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-  // Use environment variable in production (e.g., Render)
-  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-} else {
-  // Fallback to local file for development
-  serviceAccount = require("../serviceAccountKey.json");
-}
+const serviceAccount = require(serviceAccountPath);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
+
 
