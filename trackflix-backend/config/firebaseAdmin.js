17ccc83b7fa9164921d7446e13af5a647a957020
Robiul Hasan Jisan <robiulhasanjisan88@gmail.com>
2025-07-04 23:38:52 +0600

    Update firebaseAdmin.js

--- trackflix-backend/config/firebaseAdmin.js
+++ trackflix-backend/config/firebaseAdmin.js
@@ -1,16 +1,4 @@
-//const admin = require("firebase-admin");
-//const path = require("path");
 
-// Path to your Firebase service account JSON file
-//const serviceAccountPath = path.join(__dirname, "..", "serviceAccountKey.json");
-
-//const serviceAccount = require(serviceAccountPath);
-
-//admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-//});
-
-//module.exports = admin;
 
 const admin = require("firebase-admin");
 
